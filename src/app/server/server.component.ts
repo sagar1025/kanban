1a1f3b99a843c510f859695955158d87b836643b
Sagar Patel <sagar.patel@willowtreeapps.com>
2022-08-26 14:26:46 -0400

    add binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-server",
-    templateUrl: "./server.component.html"
+    templateUrl: "./server.component.html",
+    styleUrls: ["../servers/servers.component.css"] //you can reference stylesheets from other components
 })
 
-export class ServerComponent { }
+export class ServerComponent { 
+    serverId = 10;
+    serverStatus = "Offline";
+
+    getserverStatus() {
+        return this.serverStatus;
+    }
+}
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created!";
+  serverName = "";
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer(): void {
+    this.serverCreationStatus = "Server was created!";
+  }
+
+  onUpdateServerName(event: Event): void {
+    this.serverName = (<HTMLInputElement>event.target).value;
+
   }
 
 }
