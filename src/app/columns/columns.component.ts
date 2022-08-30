484cf1b29516c68bca9a937120d1360c2b1f4adc
Sagar Patel <sagar.patel@willowtreeapps.com>
2022-08-30 10:06:58 -0400

    add logic to save columns for board

--- src/app/columns/columns.component.ts
+++ src/app/columns/columns.component.ts
@@ -3,6 +3,7 @@ import { IBoard } from '../interface/IBoard';
 import { StorageService } from '../services/storageService';
 import { activeBoardKey, boardKey } from '../constants';
 import IColumn  from '../interface/IColumn';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-columns',
@@ -14,12 +15,36 @@ export class ColumnsComponent implements OnInit {
   columns: IColumn[] = [];
   constructor(private localstorage: StorageService) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    const activeBoard = <IBoard>this.localstorage.get(activeBoardKey);
+    this.columns = activeBoard.columns && activeBoard.columns.length > 0 ? activeBoard.columns : [];
+  }
 
-  addColumn() {
-    const newColumn: IColumn = {
-      id: 0,
-      name: 'asda'
+  addColumn(form: NgForm): void {
+    const formData = form.value;
+    const activeBoard = <IBoard>this.localstorage.get(activeBoardKey);
+    const allBoards = this.localstorage.get(boardKey);
+    if (activeBoard) {
+      if (activeBoard.columns) {
+        const newColumn: IColumn = {
+          id: activeBoard.columns.length,
+          name: formData.columnName
+        }
+        allBoards[activeBoard.id].columns.push(newColumn);
+        activeBoard.columns.push(newColumn);
+      }
+      else {
+        allBoards[activeBoard.id].columns = [];
+        const newColumn: IColumn = {
+          id: 0,
+          name: formData.columnName
+        }
+        allBoards[activeBoard.id].columns.push(newColumn);
+        activeBoard.columns = [newColumn];
+      }
+      this.localstorage.set(boardKey, allBoards);
+      this.localstorage.set(activeBoardKey, activeBoard);
+      window.location.reload();
     }
   }
 }
