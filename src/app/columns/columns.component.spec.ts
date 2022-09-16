807cfa044de04d0737278bee622d6cd11b49ebea
Sagar Patel <sagar.patel@willowtreeapps.com>
2022-09-16 10:23:50 -0400

    add tests for columns component

--- src/app/columns/columns.component.spec.ts
+++ src/app/columns/columns.component.spec.ts
@@ -1,23 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { StorageService } from '../services/storageService';
+import {  ACTIVE_BOARD_DATA, BOARDS_DATA } from '../services/storageService.spec';
 import { ColumnsComponent } from './columns.component';
+import { boardKey, activeBoardKey } from '../constants';
 
-xdescribe('ColumnsComponent', () => {
+describe('ColumnsComponent', () => {
   let component: ColumnsComponent;
   let fixture: ComponentFixture<ColumnsComponent>;
+  let storageService: StorageService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ColumnsComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ColumnsComponent, NavbarComponent ],
+      imports: [
+        FontAwesomeModule,
+        FormsModule, 
+        ReactiveFormsModule
+      ],
+      providers: [
+        StorageService
+      ]
+    });
+
 
     fixture = TestBed.createComponent(ColumnsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    storageService = TestBed.inject(StorageService);
+
+  });
+
+  afterEach(() => {
+    storageService.clear();
   });
 
   it('should create component', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
+  });
+
+  it('should display new column button', () => {
+    const btn = document.querySelector('.btn');
+    expect(btn).not.toBeNull();
   });
 });
